0c25fa8e9a3acbcf4f9ef91e34cdcf7e77d8ac3d
Angelo <igitegoangello@gmail.com>
2017-07-03 15:11:04 +0200

    Landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :students
-  resources :ikigos
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	resources :students
+	resources :ikigos
+
+	root to: 'ikigos#ahabanza'
+
+
 end
